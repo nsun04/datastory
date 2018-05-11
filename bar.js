f538accdc7092691b4922270f31565ee36b29ecb
Nan Sun <nsun04@syr.edu>
2018-05-11 19:25:38 -0400

    more revise

--- bar.js
+++ bar.js
@@ -1,5 +1,8 @@
 (function(){
-var svg = d3.select("svg"),
+var svg = d3.select("#dummy-bar-chart-container")
+            .append('svg')
+            .attr('width',600)
+            .attr('height',600)
     margin = {top: 20, right: 20, bottom: 30, left: 80},
     width = +svg.attr("width") - margin.left - margin.right,
     height = +svg.attr("height") - margin.top - margin.bottom;
